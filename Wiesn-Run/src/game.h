e555a9a44dabcb65135ea6b68550fa94a28304c6
Simon Gasse <simon.gasse@tum.de>
2015-06-08 22:16:32 +0200

    Game.h entsprechend des UMLs aufgebaut

--- Wiesn-Run/src/game.h
+++ Wiesn-Run/src/game.h
@@ -1,9 +1,62 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <QList>
+#include <QSortedList>
+#include <QMultiHash>
+#include gameobject.h
+#include input.h
+
+enum collisionType {
+    obstacleCollision, enemyCollision, powerupCollision
+};
+
+enum collisionDirection {
+    fromLeft, fromRight, fromAbove, fromBelow
+};
+
+struct eventStruct {
+    GameObject objectA;
+    GameObject objectB;
+    enum collisionType collision;
+    enum collisionDirection direction;
+};
+
+struct scoreStruct {
+    int enemiesKilled;
+    int distanceCovered;
+    int alcoholPoints;
+};
+
 class Game {
 public:
     Game();
+    ~Game();
+
+    QList<struct eventStruct> eventsToHandle;
+    QMultiHash states;
+
+private:
+    void startGame();
+    int getStepSize();
+    void appendWorldObjects();
+    void reduceWorldObjects();
+    void evaluateInput();
+    void calculateMovement();
+    void detectCollision();
+    void correctMovement();
+    void handleEvents();
+    void renderGraphics();
+    void playSound();
+    void endGame();
+
+    QSortedList<GameObject> worldObjects;
+    QSortedList<GameObject> levelInitial;
+    QSortedList<GameObject> levelSpawn;
+    struct scoreStruct score;
+    int stepSize;
+    GameObject *playerObjPointer;
+    Input keyInputs;
 };
 
 #endif // GAME_H
