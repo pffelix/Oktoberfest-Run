556d686cc1e48ed5685310438ac6e5b8e11e0c57
Rupert Amann <rupert.amann@tum.de>
2015-06-11 11:17:23 +0200

    Game::exec grober Ablauf im Kommentar

--- Wiesn-Run/src/game.cpp
+++ Wiesn-Run/src/game.cpp
@@ -7,6 +7,27 @@ Game::~Game() {
 
 }
 
+/**
+ * @brief Game-Loop
+ * Diese Funktion wird von main() aufgerufen und ist für den kompletten Ablauf des Spiels verantwortlich.
+ * grober Ablauf:
+ * LOOP:
+ *  - Timer starten
+ *  - Neue Objekte zur Welt hinzufügen
+ *  - alte Objekte löschen
+ *  - Input auslesesn
+ *  - Bewegungen berechnen
+ *  - Kollisionskontrolle
+ *  - Bewegungen korrigieren
+ *  - Events behandeln (Treffer..)
+ *  - Grafik rendern und ausgeben
+ *  - Sound ausgeben
+ *  - verbleibende Zeit im Slot berechnen (Timer auslesen)
+ *  - entsprechend warten
+ * goto LOOP
+ * @return 0 bei fehlerfreiem Beenden
+ * @author Rupert
+ */
 int Game::exec() {
     return 0;
 }
