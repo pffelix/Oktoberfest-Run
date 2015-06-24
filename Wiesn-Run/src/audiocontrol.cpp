0c192e8c2baa1f3b404f0f948a5c42a6e4302004
Felix Pfreundtner <humanityhour@googlemail.com>
2015-06-24 20:28:49 +0200

    wrote comments for audio.cpp update()

--- Wiesn-Run/src/audiocontrol.cpp
+++ Wiesn-Run/src/audiocontrol.cpp
@@ -19,25 +19,35 @@ AudioControl::~AudioControl() {
 
 /**
  * @brief  update
- *         update aktualisiert die intern gespeicherten audioStruct's des letzen Steps mit aktuellen audioStruct's der GameLogik.
- *         Hierfür wird die liste audioevents ausgewertet.
+ *         update aktualisiert die im Moment abgespielten, in der Liste "playevents"
+ *         gespeicherten playStruct's mit aktuellen audioStruct's aus der Liste audioevents der GameLogik.
  * @param  Qlist audioevents
  * @author  Felix Pfreundtner
  */
 void AudioControl::update(std::list<struct audioStruct> *audioevents){
+    /// erstelle neues temporäres audioStruct, welches stets das aktuelle audioStruct Element der Liste audioevents beinhaltet.
     struct audioStruct newaudiostruct;
+    /// erstelle neues temporäres audioStruct, welches stets das aktuelle playStruct Element der Liste playevents beinhaltet.
     struct playStruct newplaystruct;
+    /// lösche alte playevents Liste
     playevents.clear();
-    while (!(audioevents->empty())) {
-        newaudiostruct = *audioevents->begin();
 
+    /// iteriere über alle audioStructs der audioevents Liste
+    while (!(audioevents->empty()))
+        /// entnehme neues audioStruct aus audioevents Liste
+        newaudiostruct = *audioevents->begin();
+        /// schreibe Namen des neuen audioStruct in ein neues playStruct
         newplaystruct.name = newaudiostruct.name;
+        /// schreibe Distanz Liste des neuen audioStruct in die Volume Liste des playStruct
         newplaystruct.volume = newaudiostruct.distance;
+        /// Wandle die Distanz Liste in eine Volume Liste um (volume = 1- distanz).
         for (std::list<float>::iterator it = newplaystruct.volume.begin(); it != newplaystruct.volume.end(); it++) {
-            *it = 1 - *it;
+            *it = 1.0 - *it;
         }
-        audioevents->pop_front();
+        /// Füge neues playStruct der playevents Liste hinzu.
         playevents.push_back(newplaystruct);
-        qDebug("stop");
+        /// Lösche audioStruct aus audioevents Liste da alle Werte in playevents übernommen wurden
+        audioevents->pop_front();
+
+    qDebug("stop");
     }
-}
