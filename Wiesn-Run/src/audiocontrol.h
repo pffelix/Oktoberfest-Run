ba191178063f0b9324e678913cff957449e29f56
Felix Pfreundtner <humanityhour@googlemail.com>
2015-06-26 14:44:36 +0200

    pushed callback function from audiocontrol header to audiocontrol cpp

--- Wiesn-Run/src/audiocontrol.h
+++ Wiesn-Run/src/audiocontrol.h
@@ -48,39 +48,6 @@ public:
 
     };
 
-    /**
-     * @brief  playCallback
-     *         playCallback wird von Portaudio aufgerufen wenn nahezu alle
-     *         Audiosamples abgespielt sind und neu Audiosamples benötigt werden.
-     * @param  Qlist audioevents
-     * @author  Felix Pfreundtner
-     */
-    static int patestCallback( const void *inputBuffer, void *outputBuffer,
-                               unsigned long framesPerBuffer,
-                               const PaStreamCallbackTimeInfo* timeInfo,
-                               PaStreamCallbackFlags statusFlags,
-                               void *userData )
-    {
-        /* Cast data passed through stream to our structure. */
-        paTestData *data = (paTestData*)userData;
-        float *out = (float*)outputBuffer;
-        unsigned int i;
-        (void) inputBuffer; /* Prevent unused variable warning. */
-
-        for( i=0; i<framesPerBuffer; i++ )
-        {
-            *out++ = data->left_phase;  /* left */
-            *out++ = data->right_phase;  /* right */
-            /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-            data->left_phase += 0.01f;
-            /* When signal reaches top, drop back down. */
-            if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
-            /* higher pitch so we can distinguish left and right. */
-            data->right_phase += 0.03f;
-            if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
-        }
-        return 0;
-    }
 
 
     AudioControl();
@@ -88,7 +55,12 @@ public:
 
     void update(std::list<struct audioStruct> *audioevents);
 
-private:
+    static int patestCallback( const void *inputBuffer, void *outputBuffer,
+                               unsigned long framesPerBuffer,
+                               const PaStreamCallbackTimeInfo* timeInfo,
+                               PaStreamCallbackFlags statusFlags,
+                               void *userData );
+    private:
     /**
      * @brief  playevents
      *         playevents beinhaltet eine Liste mit allen im Moment abgespielten playStructs.
