b47e3f42d1fd5cf8013e6b9553104ce7e5ac3364
Felix Pfreundtner <humanityhour@googlemail.com>
2015-06-30 19:25:52 +0200

    added more comments to getkeyletters

--- Wiesn-Run/src/input.cpp
+++ Wiesn-Run/src/input.cpp
@@ -435,7 +435,9 @@ QSet<int> Input::getKeyactions() {
  *         Jeder Buchststaben Taste wird ein String Buchstaben zugeordnet, welcher im QSet keyletters gespeichert ist.
  *         Über die Enumeration Input::Keyletter ist jeder Buchstabe mit dem zugehörigen Indize in keyletters verknüft.
  *         Möchte man nun bespielsweise abfragen ob der Spieler im Moment die "a" Taste drückt so überprüft man:
- *         input->getKeyletters().find(Input::Keyaction::Shoot) != getKeyletters().end().
+ *         input->getKeyletters().find(Input::Keyletter::a) != getKeyletters().end().
+ *         Möchte man abfragen ob der Spieler im Moment die "A" Taste drückt so überprüft man:
+ *         input->getKeyletters().find(Input::Keyletter::A) != getKeyletters().end().
  * @return Std::Set Instanzvariable keyactions
  * @author Felix Pfreundtner
  */
