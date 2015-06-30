8d87560582ddc3a2a8bdf7291336e8a406fbff1d
Felix Pfreundtner <humanityhour@googlemail.com>
2015-06-30 18:52:01 +0200

    added whole alphabet for keyboard input

--- Wiesn-Run/src/input.cpp
+++ Wiesn-Run/src/input.cpp
@@ -111,10 +111,307 @@ void Input::updateKeys() {
             lastKeyletter = Keyletter::a;
             qDebug("a");
         }
-
     }
-
-
+    if(keyevents.contains(Qt::Key_B)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::B);
+            lastKeyletter = Keyletter::B;
+            qDebug("B");
+        }
+        else {
+            keyletters.insert((char)Keyletter::b);
+            lastKeyletter = Keyletter::b;
+            qDebug("b");
+        }
+    }
+    if(keyevents.contains(Qt::Key_C)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::C);
+            lastKeyletter = Keyletter::C;
+            qDebug("C");
+        }
+        else {
+            keyletters.insert((char)Keyletter::c);
+            lastKeyletter = Keyletter::c;
+            qDebug("c");
+        }
+    }
+    if(keyevents.contains(Qt::Key_D)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::D);
+            lastKeyletter = Keyletter::D;
+            qDebug("D");
+        }
+        else {
+            keyletters.insert((char)Keyletter::d);
+            lastKeyletter = Keyletter::d;
+            qDebug("d");
+        }
+    }
+    if(keyevents.contains(Qt::Key_E)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::E);
+            lastKeyletter = Keyletter::E;
+            qDebug("E");
+        }
+        else {
+            keyletters.insert((char)Keyletter::e);
+            lastKeyletter = Keyletter::e;
+            qDebug("e");
+        }
+    }
+    if(keyevents.contains(Qt::Key_F)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::F);
+            lastKeyletter = Keyletter::F;
+            qDebug("F");
+        }
+        else {
+            keyletters.insert((char)Keyletter::f);
+            lastKeyletter = Keyletter::f;
+            qDebug("f");
+        }
+    }
+    if(keyevents.contains(Qt::Key_G)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::G);
+            lastKeyletter = Keyletter::G;
+            qDebug("G");
+        }
+        else {
+            keyletters.insert((char)Keyletter::g);
+            lastKeyletter = Keyletter::g;
+            qDebug("g");
+        }
+    }
+    if(keyevents.contains(Qt::Key_H)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::H);
+            lastKeyletter = Keyletter::H;
+            qDebug("H");
+        }
+        else {
+            keyletters.insert((char)Keyletter::h);
+            lastKeyletter = Keyletter::h;
+            qDebug("h");
+        }
+    }
+    if(keyevents.contains(Qt::Key_I)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::I);
+            lastKeyletter = Keyletter::I;
+            qDebug("I");
+        }
+        else {
+            keyletters.insert((char)Keyletter::i);
+            lastKeyletter = Keyletter::i;
+            qDebug("i");
+        }
+    }
+    if(keyevents.contains(Qt::Key_J)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::J);
+            lastKeyletter = Keyletter::J;
+            qDebug("J");
+        }
+        else {
+            keyletters.insert((char)Keyletter::j);
+            lastKeyletter = Keyletter::j;
+            qDebug("j");
+        }
+    }
+    if(keyevents.contains(Qt::Key_K)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::K);
+            lastKeyletter = Keyletter::K;
+            qDebug("K");
+        }
+        else {
+            keyletters.insert((char)Keyletter::k);
+            lastKeyletter = Keyletter::k;
+            qDebug("k");
+        }
+    }
+    if(keyevents.contains(Qt::Key_L)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::L);
+            lastKeyletter = Keyletter::L;
+            qDebug("L");
+        }
+        else {
+            keyletters.insert((char)Keyletter::l);
+            lastKeyletter = Keyletter::l;
+            qDebug("l");
+        }
+    }
+    if(keyevents.contains(Qt::Key_M)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::M);
+            lastKeyletter = Keyletter::M;
+            qDebug("M");
+        }
+        else {
+            keyletters.insert((char)Keyletter::m);
+            lastKeyletter = Keyletter::m;
+            qDebug("m");
+        }
+    }
+    if(keyevents.contains(Qt::Key_N)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::N);
+            lastKeyletter = Keyletter::N;
+            qDebug("N");
+        }
+        else {
+            keyletters.insert((char)Keyletter::n);
+            lastKeyletter = Keyletter::n;
+            qDebug("n");
+        }
+    }
+    if(keyevents.contains(Qt::Key_O)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::O);
+            lastKeyletter = Keyletter::O;
+            qDebug("O");
+        }
+        else {
+            keyletters.insert((char)Keyletter::o);
+            lastKeyletter = Keyletter::o;
+            qDebug("o");
+        }
+    }
+    if(keyevents.contains(Qt::Key_P)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::P);
+            lastKeyletter = Keyletter::P;
+            qDebug("P");
+        }
+        else {
+            keyletters.insert((char)Keyletter::p);
+            lastKeyletter = Keyletter::p;
+            qDebug("p");
+        }
+    }
+    if(keyevents.contains(Qt::Key_Q)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::Q);
+            lastKeyletter = Keyletter::Q;
+            qDebug("Q");
+        }
+        else {
+            keyletters.insert((char)Keyletter::q);
+            lastKeyletter = Keyletter::q;
+            qDebug("q");
+        }
+    }
+    if(keyevents.contains(Qt::Key_R)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::R);
+            lastKeyletter = Keyletter::R;
+            qDebug("R");
+        }
+        else {
+            keyletters.insert((char)Keyletter::r);
+            lastKeyletter = Keyletter::r;
+            qDebug("r");
+        }
+    }
+    if(keyevents.contains(Qt::Key_S)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::S);
+            lastKeyletter = Keyletter::S;
+            qDebug("S");
+        }
+        else {
+            keyletters.insert((char)Keyletter::s);
+            lastKeyletter = Keyletter::s;
+            qDebug("s");
+        }
+    }
+    if(keyevents.contains(Qt::Key_T)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::T);
+            lastKeyletter = Keyletter::T;
+            qDebug("T");
+        }
+        else {
+            keyletters.insert((char)Keyletter::t);
+            lastKeyletter = Keyletter::t;
+            qDebug("t");
+        }
+    }
+    if(keyevents.contains(Qt::Key_U)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::U);
+            lastKeyletter = Keyletter::U;
+            qDebug("U");
+        }
+        else {
+            keyletters.insert((char)Keyletter::u);
+            lastKeyletter = Keyletter::u;
+            qDebug("u");
+        }
+    }
+    if(keyevents.contains(Qt::Key_V)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::V);
+            lastKeyletter = Keyletter::V;
+            qDebug("V");
+        }
+        else {
+            keyletters.insert((char)Keyletter::v);
+            lastKeyletter = Keyletter::v;
+            qDebug("v");
+        }
+    }
+    if(keyevents.contains(Qt::Key_W)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::W);
+            lastKeyletter = Keyletter::W;
+            qDebug("W");
+        }
+        else {
+            keyletters.insert((char)Keyletter::w);
+            lastKeyletter = Keyletter::w;
+            qDebug("w");
+        }
+    }
+    if(keyevents.contains(Qt::Key_X)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::X);
+            lastKeyletter = Keyletter::X;
+            qDebug("X");
+        }
+        else {
+            keyletters.insert((char)Keyletter::x);
+            lastKeyletter = Keyletter::x;
+            qDebug("x");
+        }
+    }
+    if(keyevents.contains(Qt::Key_Y)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::Y);
+            lastKeyletter = Keyletter::Y;
+            qDebug("Y");
+        }
+        else {
+            keyletters.insert((char)Keyletter::y);
+            lastKeyletter = Keyletter::y;
+            qDebug("y");
+        }
+    }
+    if(keyevents.contains(Qt::Key_Z)) {
+        if(keyevents.contains(Qt::Key_Shift)) {
+            keyletters.insert((char)Keyletter::Z);
+            lastKeyletter = Keyletter::Z;
+            qDebug("Z");
+        }
+        else {
+            keyletters.insert((char)Keyletter::z);
+            lastKeyletter = Keyletter::z;
+            qDebug("z");
+        }
+    }
 
 }
 
