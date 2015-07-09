e9e99a2a044c9b6076bee4a1176a1cbae7f537a4
Felix Pfreundtner <humanityhour@googlemail.com>
2015-07-09 21:05:16 +0200

    added drunken sound

--- Wiesn-Run/src/definitions.h
+++ Wiesn-Run/src/definitions.h
@@ -72,7 +72,7 @@ const int maxSpeedY = 3 * maxSpeed / 2;
  * @brief maximales Leben
  */
 const int maxHealth = 5;
-const int maxAlcohol = 4500;
+const int maxAlcohol = 200;
 
 /**
  * @brief PowerUp-Konstanten
