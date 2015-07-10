66293b4af6606b36e9e5de714f3f606982aacddc
Simon Gasse <simon.gasse@tum.de>
2015-07-10 15:06:16 +0200

    Level3 startk überarbeitet und python-Skript für Planes

---
+++ Wiesn-Run/src/levelFiles/createPlanes.py
@@ -0,0 +1,38 @@
+'''
+Usage:
+ipython
+run createPlanes.py
+L = Levelhelper()
+L.makePlanes(12345,130,10)
+    -> copy to Levelfile
+'''
+
+
+
+class Levelhelper:
+    'Encapsulates the functionality to generate strings for the Wiesn-Run levels.'
+
+    helper_Count = 0
+
+    def __init__(self):
+        Levelhelper.helper_Count += 1
+
+    def makePlanes(self, startPos, height, number, mode='print'):
+        plane_str = "Plane," + str(startPos) + "," + str(height) + "\n"
+        for i in range(1, number):
+            plane_str = plane_str + "Plane," + str(startPos + i*120) + "," + str(height) + "\n"
+
+        if mode == 'print':
+            print plane_str
+        else:
+            return plane_str 
+
+    def makeTwoWalkways(self, startPos, height1, height2, number, mode_W='print'):
+        plane_str = self.makePlanes(startPos, height1, number, mode='return')
+        plane_str = plane_str + self.makePlanes(startPos + 120, height2, number - 1, mode='return')
+
+        if mode_W == 'print':
+            print plane_str
+        else:
+            return plane_str
+
