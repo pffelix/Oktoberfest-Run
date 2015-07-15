4824bed67d92e2b21dac25ccfd47fa441c2378fe
Felix Pfreundtner <humanityhour@googlemail.com>
2015-07-15 09:55:01 +0200

    updated doxygen comments

--- Wiesn-Run/src/audio.h
+++ Wiesn-Run/src/audio.h
@@ -19,7 +19,7 @@
 
 /**
  * @brief  Die Audio Klasse erzeugt Audioobjekte, welche Wave Samples und deren Meta Information speichern. Für jedes Audio Wave File im Ordner Audio (AudioEventgruppe audioType) wird zum Spielstart in Game über den Konstruktor des AudioControl Objekt audioOutput Audio(std::string type_name) eine Instanz der Klasse erstellt und das erstellte Objekt in audioOutput an eine Liste audioobjects angehängt. Bei der Erstellung liest das Audioobjekt die zum Audio Objekt gehörigen Samples byteweise aus der gespeicherten WAVE Datei in den Vektor samples ein, wobei die Samples normalisiert werden. Das Eingabeformat sind Einkanal little-endian RIFF Wave Dateien mit 16 bit oder 8 bit Bittiefe und einer Samplerate von 44100 Hz. Das audioControl Objekt audioOutput kann auf ein bestimmtes Sample über die Methode getSample(int pos) zugreifen. Die Anzahl aller Samples im Vektor samples kann über getSamplenumber() abgefragt werden.
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 class Audio{
 
--- Wiesn-Run/src/input.cpp
+++ Wiesn-Run/src/input.cpp
@@ -2,7 +2,7 @@
 
 /**
  * @brief  Konstruktor instanziiert ein Objekt der Klasse Input. Wird einmal zum Spielstart von dem game Objekt aufgerufen und ein Objekt keyInput erstellt.
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 Input::Input() {
 }
@@ -10,7 +10,7 @@ Input::Input() {
 
 /**
  * @brief  Destruktor löscht ein Objekt der Klasse Input
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 Input::~Input() {
 }
@@ -18,7 +18,7 @@ Input::~Input() {
 /**
  * @brief  Nach Aufruf über Game::eventFilter wertet die Methode evaluatekeyEvent alle im Momment gleichzeitig gepressten Tastatur Eingaben aus und speichert die zugehörigen enum ids in der Instanzvariable keyevents. Wird eine Taste nicht mehr gedrück wird die enum id in keyevents gelöscht. Wird eine Taste neu gedrückt wird die enum id in keyevents hinzugefügt. Um die relevanten Tastaturkombinationen auszuwerten wird die Methode updateKeys aufgerufen.
  * @param  QEvent *event
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 
 void Input::evaluatekeyEvent(QEvent *event) {
@@ -38,7 +38,7 @@ void Input::evaluatekeyEvent(QEvent *event) {
 
 /**
  * @brief  Die Methode updateKeys berechnet aus allen in keyevents gespeicherten Tastatureingaben die für das Spiel relevanten Kombinationen und speichert diese im QSet keyactions oder set keyletters. Jede aktuell gedrückte Tastaturkombination ist im QSet keyactions als Integer gespeichert, welche über die enumeration Keyaction adressiert wird. Jeder aktuell gedrückte Buchstabe ist im set keyletters als char gespeichert, welche über die enumeration Keyletter und einen int to char Typecast adressiert wird. Wird durch die Methode evaluatekeyEvent ein KeyRelease oder KeyPress Event aufgezeichnet, so nach Aufruf der Methode das QSet keyactions und set keyletters gelöscht. Im Anschluss wird geprüft ob in der neuen Situation in keyevents relevante Tastaturkombinationen vorhanden sind. Sind Tasten oder Tastenkombinationen gedrückt worden, welche für das Spiel relevant sind so wird die zur Aktion gehörige enum Keyaction integer ID im QSet keyactions hinzugefügt. Ist ein Buchstabe in keyevents gespeichert, so wird der zugehörige char im set keyletters gespeichert.
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 void Input::updateKeys() {
     // update alle für das Gameplay relevante Tasten(-kombinationen)
@@ -401,7 +401,7 @@ void Input::updateKeys() {
 /**
  * @brief  Die Methode getKeyactions gibt bei Aufruf das QSet keyactions zurück, welches alle im Moment gedrückten Spielaktionen als Enum beinhaltet. Jeder Tastaturkombination wird eine Integer ID zugeordnet welche im QSet keyactions gespeichert ist. Über die Enumeration Input::Keyaction ist jeder Spielbefehl mit dem zugehörigen Index in keyactions verknüft. Möchte man nun bespielsweise abfragen ob der Spieler im Moment schießt so überprüft man: input->getKeyactions().contains(Input::Keyaction::Shoot) == True.
  * @return  QSet<int> Instanzvariable keyactions
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 QSet<int> Input::getKeyactions() {
     return keyactions;
@@ -410,7 +410,7 @@ QSet<int> Input::getKeyactions() {
 /**
  * @brief  Die Methode getKeyletters gibt bei Aufruf das set keyletters zurück, welches alle im Moment gedrückten Buchstaben als Enum beinhaltet. Jeder Buchststaben Taste wird ein String Buchstaben zugeordnet, welcher im set keyletters gespeichert ist. Über die Enumeration Input::Keyletter ist jeder Buchstabe mit dem zugehörigen Index in keyletters verknüft.
  * @return  std::set<char> Instanzvariable keyletters
- * @author  Felix Pfreundtner
+ * @author  Felix
  */
 std::set<char> Input::getKeyletters() {
     return keyletters;
